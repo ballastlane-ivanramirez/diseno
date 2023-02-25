30f5290f358edc3ede6e228237c0f702f619d613
Ivan Ramirez <ivanramirez@ballastlane.com>
2023-02-25 12:25:21 -0500

    added post endpoint and db connection

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const { Sequelize } = require('sequelize');
+
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.json({body: "hello, diseno"}).status(400)
+})
+
+app.post('/', async (req, res) => {
+    const sequelize = new Sequelize('postgres://postgres:postgres@database-1.cu1uurva19bz.us-east-1.rds.amazonaws.com:5432/postgres') 
+    try {
+        await sequelize.authenticate();
+        const [results, metadata] = await sequelize.query("INSERT INTO public.diseno(id, num) VALUES (2,300);");
+        console.log(results)
+        return res.json({ok: true, message: "updated value correctly"}).statusCode(200)
+      } catch (error) {
+        console.error('Unable to connect to the database:', error);
+        return res.json({ok: false, message: "could not update value"}).statusCode(200)
+      }
 })
 
 app.listen(port, () => {
